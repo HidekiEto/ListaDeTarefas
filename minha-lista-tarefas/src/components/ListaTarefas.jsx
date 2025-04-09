33be9e0b0d0c4032d424109726b7900acec0d040
Hideki Eto <vitoreto07@gmail.com>
2025-04-09 08:33:52 -0300

    checkbox e ordenação

--- minha-lista-tarefas/src/components/ListaTarefas.jsx
+++ minha-lista-tarefas/src/components/ListaTarefas.jsx
@@ -9,14 +9,20 @@ function ListaTarefas(){
 
     const adicionarTarefa = () => {
         if (novaTarefa.trim() != '') {
+
             setTarefas([...tarefas, novaTarefa]);
-            setNovaTarefa("");
+            setNovaTarefa('');
         }
     };
 
     const removerTarefa = (indice) => {
         setTarefas(tarefas.filter((_, i) => i !== indice));
     };
+
+    const ordenarTarefas = () => {
+        const tarefasOrdenadas = [...tarefas].sort((a, b) => a.localeCompare(b));
+        setTarefas(tarefasOrdenadas);
+    }
     return (
         <div>
             <h1 className='subtitle'>Lista de Tarefas</h1>
@@ -28,10 +34,14 @@ function ListaTarefas(){
                 placeholder="Digite uma nova tarefa"
             />
             <button onClick={adicionarTarefa}>Adicionar</button>
+            <button onClick={ordenarTarefas}>Ordenar</button>
+           
             </div>
             <ul>
+                {tarefas.length === 0 && <p className='sem-tarefas'>Nenhuma tarefa adicionada.</p>}
                 {tarefas.map((tarefa, indice) => (
                     <li key={indice}>
+                        <input type="checkbox" className='checkbox'/>
                         {tarefa}
                         <button onClick={() => removerTarefa(indice)}>Remover</button>
                     </li>
